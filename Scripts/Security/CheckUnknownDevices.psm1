cead4b63f33ea81659f63ebaa0ea97d8d9342e98
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:17:29 +0100

    Update CheckUnknownDevices.psm1

--- Scripts/Security/CheckUnknownDevices.psm1
+++ Scripts/Security/CheckUnknownDevices.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 $toegestane_ips = @("192.168.1.1", "192.168.1.2", "192.168.1.10")  # Voeg bekende apparaten toe
 $huidige_apparaten = @()
 
