a4100826d8fa60828c87f061392758be07cd7b3a
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:15:16 +0100

    Update MacProducer.psm1

--- Scripts/Monitoring/MacProducer.psm1
+++ Scripts/Monitoring/MacProducer.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 arp -a | ForEach-Object {
     if ($_ -match "(\d+\.\d+\.\d+\.\d+)\s+([a-fA-F0-9:-]+)") {
         Write-Host "IP: $($matches[1]) - MAC: $($matches[2])"
