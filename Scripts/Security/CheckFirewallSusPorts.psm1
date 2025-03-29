28b4c87601125f8c80a7bcc0b8cf16fb28bd757f
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:16:55 +0100

    Update CheckFirewallSusPorts.psm1

--- Scripts/Security/CheckFirewallSusPorts.psm1
+++ Scripts/Security/CheckFirewallSusPorts.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 $blacklist_poorten = @(21, 23, 25, 3389) # Pas aan op basis van je beleid
 
 Get-NetFirewallRule | Where-Object { $_.Enabled -eq "True" } | ForEach-Object {
