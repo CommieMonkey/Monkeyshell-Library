5aae6eb1cf7d06a8d03b0863ecd63454fd2425a0
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:17:05 +0100

    Update CheckMFAonAD.psm1

--- Scripts/Security/CheckMFAonAD.psm1
+++ Scripts/Security/CheckMFAonAD.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 Import-Module ActiveDirectory
 
 Get-ADUser -Filter * -Properties * | Where-Object { $_.SmartcardLogonRequired -ne $true } | Select-Object Name, UserPrincipalName
