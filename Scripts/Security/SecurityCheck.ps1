17bc89e3e333c1288bbee0a0819e78a65b5aae00
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:18:02 +0100

    Update SecurityCheck.ps1

--- Scripts/Security/SecurityCheck.ps1
+++ Scripts/Security/SecurityCheck.ps1
@@ -1,3 +1,11 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
+
 function Toon-Menu {
     Clear-Host
     Write-Host "============================="
