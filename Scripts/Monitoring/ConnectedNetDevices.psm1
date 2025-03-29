4cf10a615959afb62fb598352cefc646225e0f8e
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:03:05 +0100

    Update ConnectedNetDevices.psm1

--- Scripts/Monitoring/ConnectedNetDevices.psm1
+++ Scripts/Monitoring/ConnectedNetDevices.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 $subnet = "192.168.1"  # Pas dit aan naar jouw netwerk
 1..254 | ForEach-Object {
     $ip = "$subnet.$_"
