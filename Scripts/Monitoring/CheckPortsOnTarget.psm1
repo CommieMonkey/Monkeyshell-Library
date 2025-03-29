f8428cfe438039312c83bf557fe3b33fb4940dcb
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:02:52 +0100

    Update CheckPortsOnTarget.psm1

--- Scripts/Monitoring/CheckPortsOnTarget.psm1
+++ Scripts/Monitoring/CheckPortsOnTarget.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 $ip = "192.168.1.1"  # Pas dit aan naar de gewenste host
 $poorten = @(22, 80, 443, 3389)  # Voeg meer poorten toe indien nodig
 
