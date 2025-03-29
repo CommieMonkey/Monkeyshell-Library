937728cfba23d643f69226dd702d5600cdca5d88
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:03:45 +0100

    Update DomainIPcheck.psm1

--- Scripts/Monitoring/DomainIPcheck.psm1
+++ Scripts/Monitoring/DomainIPcheck.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 $domeinen = @("google.com", "192.168.1.1", "mijnserver.local")
 foreach ($domein in $domeinen) {
     if (Test-Connection -ComputerName $domein -Count 2 -Quiet) {
