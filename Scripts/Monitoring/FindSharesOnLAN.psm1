dd8e50011609a41d149bd028804d0f4fbddf1753
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:04:11 +0100

    Update FindSharesOnLAN.psm1

--- Scripts/Monitoring/FindSharesOnLAN.psm1
+++ Scripts/Monitoring/FindSharesOnLAN.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 $computers = (Get-ADComputer -Filter *).Name  # Vereist Active Directory
 foreach ($computer in $computers) {
     Write-Host "Gedeelde mappen op $computer:"
