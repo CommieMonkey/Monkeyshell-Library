1533e656eac810040de226f80d573bb2c1a4670c
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:17:16 +0100

    Update CheckSharesWeakPerms.psm1

--- Scripts/Security/CheckSharesWeakPerms.psm1
+++ Scripts/Security/CheckSharesWeakPerms.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 Get-SmbShare | ForEach-Object {
     Write-Host "Share: $($_.Name) - Toegang: $($_.Path)"
     Get-SmbShareAccess -Name $_.Name
