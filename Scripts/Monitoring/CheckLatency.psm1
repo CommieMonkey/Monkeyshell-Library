b34abf46005893690611aab8b7ecf9dbdc0b18aa
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:02:37 +0100

    Update CheckLatency.psm1

--- Scripts/Monitoring/CheckLatency.psm1
+++ Scripts/Monitoring/CheckLatency.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 $server = "8.8.8.8"
 $pingResults = Test-Connection -ComputerName $server -Count 5
 $avgLatency = ($pingResults | Measure-Object ResponseTime -Average).Average
