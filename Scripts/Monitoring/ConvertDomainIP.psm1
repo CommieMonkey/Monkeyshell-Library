86b4566707ef2fb5a52b461f010900f3e169737b
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:03:33 +0100

    Update ConvertDomainIP.psm1

--- Scripts/Monitoring/ConvertDomainIP.psm1
+++ Scripts/Monitoring/ConvertDomainIP.psm1
@@ -1,3 +1,10 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 $domein = "google.com"
 $ip = [System.Net.Dns]::GetHostAddresses($domein) | Select-Object -ExpandProperty IPAddressToString
 Write-Host "$domein heeft IP-adres: $ip"
