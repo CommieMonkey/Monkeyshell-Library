d6b2728529850bf339d11d24ea3f777b6f525875
CommieMonkey <80454356+CommieMonkey@users.noreply.github.com>
2025-03-29 11:15:28 +0100

    Update NetsharesOnHost.psm1

--- Scripts/Monitoring/NetsharesOnHost.psm1
+++ Scripts/Monitoring/NetsharesOnHost.psm1
@@ -1,2 +1,9 @@
+<# 
+Author: Thomas Van Aert
+License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+Description: [Brief script description]
+Note: You may NOT modify or redistribute this script without my permission.
+#>
+
 $computer = "192.168.1.1"  # Pas dit aan
 net view \\$computer
